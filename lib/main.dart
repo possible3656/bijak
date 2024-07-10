289bd9807380f6a7a43dac32a7347b53ca47c3d8
Sambhav Jain <sj94611@gmail.com>
2024-07-11 00:14:08 +0530

    feat: project initialized

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:bijak/app/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
+
+void main() {
+  runApp(
+    GetMaterialApp(
+      title: "Bijak",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+      theme: AppTheme.appTheme,
+    ),
+  );
+}
